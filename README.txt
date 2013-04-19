5461825a19383c53c52ba18dd194dc264d9bc353
Andrea D'Este <andrea.deste@redomino.com>
2013-04-19 19:57:55 +0400

    checkout, portlets and cart are now fully responsive

--- README.txt
+++ README.txt
@@ -1,8 +1,18 @@
-What is it?
+Introducion
 ===========
 
-This is the default theme for `LFS`_, an online shop based on Django.
+This is a responsive theme for LFS based on twitter bootstrap.
+LFS: http://getlfs.com
+twitter bootstrap: http://twitter.github.io/bootstrap/
 
-See `LFS`_ for more information.
+Now we have:
+ - responsive two/three columns layout
+ - only bootstrap css are active
+ - fluid category view showing all products or a table of them (depending on row and column properties).
+ - product view is now responsive
+ - static homepage is responsive
+ - applied bootstrap style at many elements like: main navigation, left navigation, search and others buttons.
+ - main navigation now shows only first level categories
 
-.. _`LFS`: http://pypi.python.org/pypi/django-lfs
+
+
--- lfs_theme/templatetags/lfs_theme_tags.py
+++ lfs_theme/templatetags/lfs_theme_tags.py
@@ -3,6 +3,10 @@ from django.core.cache import cache
 from django.template import Library, Node, TemplateSyntaxError
 from django.utils.translation import ugettext as _
 
+#from django.form.util import ErrorList as OriginalErrorList
+#from django.utils.encoding import force_unicode
+#from django.utils.safestring import mark_safe
+
 # portlets imports
 import portlets.utils
 from portlets.models import Slot
@@ -12,6 +16,15 @@ import lfs.core.utils
 from lfs.catalog.models import Category
 
 register = Library()
+
+#class ErrorList(OriginalErrorList):
+#    """ """
+#    def as_ul(self):
+#        if not self: return u''
+#        return mark_safe(u'<ul class="errorlist">%s</ul>'
+#                % ''.join([u'<li>%s%s</li>' % (k, force_unicode(v))
+#                    for k, v in self.items()]))
+
 
 class SlotsInformationNode(Node):
     """
